e240e4d3859d0297451962205abd94c4cc259e9c
mr.rob.byrne@gmail.com <mr.rob.byrne@gmail.com>
2014-10-22 23:29:21 -0400

    Delete

--- Lab11.java
+++
@@ -1,7 +0,0 @@
-public class Lab11
-  {
-  public static void main (String[]args)
-    {
-    System.out.println("You Talkin' To Me? \nTheres noone else here... \nso you must be talkin' to me!");
-    }
- }
